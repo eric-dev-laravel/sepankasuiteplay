81d25d7bb89350dce150c0d4c0bab296c194835c
eric-dev-laravel <54290874+eric-dev-laravel@users.noreply.github.com>
2019-09-20 17:51:13 -0500

    Login an Signup screen

--- app/src/main/java/com/sepankasuite/sepankaplay/splash_activity.java
+++ app/src/main/java/com/sepankasuite/sepankaplay/splash_activity.java
@@ -23,7 +23,7 @@ public class splash_activity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splash_activity.this, MainActivity.class);
+                Intent intent = new Intent(splash_activity.this, login_activity.class);
                 startActivity(intent);
                 finish();
             };
