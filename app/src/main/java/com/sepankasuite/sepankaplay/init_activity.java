d9ea31d418ae5d96e2e5df1e742e18bc1e74bd49
eric-dev-laravel <54290874+eric-dev-laravel@users.noreply.github.com>
2019-10-07 15:38:19 -0500

    Notifications Beta 1.0

--- app/src/main/java/com/sepankasuite/sepankaplay/init_activity.java
+++ app/src/main/java/com/sepankasuite/sepankaplay/init_activity.java
@@ -1,13 +1,22 @@
 package com.sepankasuite.sepankaplay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class init_activity extends AppCompatActivity implements View.OnClickListener {
 
@@ -37,6 +46,26 @@ public class init_activity extends AppCompatActivity implements View.OnClickList
         btn_registrarse.setOnClickListener(this);
 
         cursor = manager.selectDataUsers();
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        String TAG = "Token: ";
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = TAG + token;
+                        Log.d(TAG, msg);
+                        Toast.makeText(init_activity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     //Metodo que recupera que elemento fue dado clic
